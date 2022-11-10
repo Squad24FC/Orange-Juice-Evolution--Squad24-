89535dce5deaa19d7342eba306461502db4aad9e
Erick Valente <erickhwk@gmail.com>
2022-11-10 16:50:19 -0300

    feat: create track model, controller and view

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,49 @@
+class TracksController < ApplicationController
+
+  def index
+    @tracks = Track.all
+  end
+
+  def new
+    @track = Track.new
+  end
+
+  def create
+    @track = Track.new(track_params)
+    if @track.save
+      redirect_to track_path(@track)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @track = Track.find(params[:id])
+  end
+
+  def edit
+    @track = Track.find(params[:id])
+  end
+
+  def update
+    @track = Track.find(params[:id])
+    if @track.update(track_params)
+      redirect_to track_path(@track)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @track = Track.find(params[:id])
+    @track.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:name, :icon, :description)
+  end
+end
