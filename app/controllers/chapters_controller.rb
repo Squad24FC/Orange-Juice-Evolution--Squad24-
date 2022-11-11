20d70656f5c0e51c1a74fb0a3183b3c784bf9d13
Erick Valente <erickhwk@gmail.com>
2022-11-11 17:41:49 -0300

    feat: nest lesson into subject

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -2,11 +2,11 @@ class ChaptersController < ApplicationController
     def index
         @chapters = Chapter.all
       end
-    
+
       def new
         @chapter = Chapter.new
       end
-    
+
       def create
         @chapter = Chapter.new(chapter_params)
         if @chapter.save
@@ -15,15 +15,15 @@ class ChaptersController < ApplicationController
           render :new
         end
       end
-    
+
       def show
         @chapter = Chapter.find(params[:id])
       end
-    
+
       def edit
         @chapter = Chapter.find(params[:id])
       end
-    
+
       def update
         @chapter = Chapter.find(params[:id])
         if @chapter.update(chapter_params)
@@ -32,16 +32,16 @@ class ChaptersController < ApplicationController
           render :new
         end
       end
-    
+
       def destroy
         @chapter = Chapter.find(params[:id])
         @chapter.destroy
-    
+
         redirect_to chapters_path
       end
-    
+
       private
-    
+
       def chapter_params
         params.require(:chapter).permit(:name, :thumb_url)
       end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tracks
-  resources :lessons
-  resources :subjects
   resources :chapters
-
+  resources :subjects do
+    resources :lessons
+  end
   root to: 'tracks#index'
 end
