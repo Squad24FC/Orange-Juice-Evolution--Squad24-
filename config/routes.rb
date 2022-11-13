a0af4d3141017e6c83a10a9a918741a4c351fc57
brunosas88 <bruno.sas@live.com>
2022-11-13 10:21:42 -0300

    refactor: improve routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tracks do
-    resources :chapters, except: %i[index show new create edit update destroy] do
-      resources :subjects, except: %i[index show new create edit update destroy] do
+    resources :chapters, only: %i[] do
+      resources :subjects, only: %i[] do
         resources :lessons, only: %i[index]
       end
     end
