4fffdc70f867215cbdbfa1e4fc9a810622a6deeb
brunosas88 <bruno.sas@live.com>
2022-11-12 23:06:21 -0300

    feat: create nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tracks
-  resources :chapters do
-    resources :subjects do
-      resources :lessons
+  resources :tracks do
+    resources :chapters, except: %i[index show new create edit update destroy] do
+      resources :subjects, except: %i[index show new create edit update destroy] do
+        resources :lessons, only: %i[index]
+      end
     end
   end
   root to: 'tracks#index'
